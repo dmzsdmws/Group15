914716b9c0b04d9feaf4bb1e487127d2de39c47d
dmzsdmws <103725364+dmzsdmws@users.noreply.github.com>
2023-01-16 20:06:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ moji = st.text_input("検索したい文字:",key="moji",value = "irofude")
 if st.session_state.moji is not None:
    search = moji 
 image = Image.open(search +".jpg")
+st.write("検索結果")
 st.image(image,width = 200)
 st.write("低学年")
 moji1 = Image.open('images/iro1.jpg')
