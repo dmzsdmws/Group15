7a99c09ae5d711322c88af7fb72fbcbf7e17242f
dmzsdmws <103725364+dmzsdmws@users.noreply.github.com>
2023-01-16 18:55:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 # 以下を「app.py」に書き込み
 import streamlit as st
+import streamlit.components.v1 as components
 from PIL import Image
 import numpy as np
 import pandas as pd
 st.title("   "  + "GROUP15 -IROFUDE-")
-st.markdown("This text is :red[colored red], and this is **:blue[colored]** and bold.")
+st.markdown('<style>div[class="css-6qob1r e1fqkh3o3"] { background: url("https://media2.giphy.com/media/46hpy8xB3MiHfruixn/giphy.gif");background-repeat: no-repeat;background-size:350%;border:1px solid #36454F; border-top:none;} </style>', unsafe_allow_html=True)
 st.title(" ")
 moji = st.text_input("検索したい文字",key="moji",value = "irofude")
 if st.session_state.moji is not None:
