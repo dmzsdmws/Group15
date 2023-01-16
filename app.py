63c4bc1898b736a121e000295dee088a439e8c6c
dmzsdmws <103725364+dmzsdmws@users.noreply.github.com>
2023-01-16 19:21:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,20 @@ import streamlit.components.v1 as components
 from PIL import Image
 import numpy as np
 import pandas as pd
-st.markdown("""
- <style> 
- background : blue
- </style>""", unsafe_allow_html=True)
+css = '''
+.stApp {
+    background: url('https://wallpapercave.com/wp/wp9597778.jpg');
+    background-size: cover;
+    background-position: center;
+}
+.stApp > header {
+    background-color: transparent;
+}
+
+'''
+
+st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+
 st.title("   "  + "GROUP15 -IROFUDE-")
 st.title(" ")
 moji = st.text_input("検索したい文字",key="moji",value = "irofude")
@@ -18,13 +28,13 @@ if st.session_state.moji is not None:
    search = moji 
 image = Image.open(search +".jpg")
 st.image(image,width = 200)
-st.markdown(":blue[低学年]")
+st.write("低学年")
 moji1 = Image.open('images/iro1.jpg')
 st.image(moji1,width = 50)
-st.markdown(":blue[中学年]")
+st.write("中学年")
 moji2 = Image.open('images/iro2.jpg')
 st.image(moji2,width = 50)
-st.markdown(":blue[高学年]")
+st.write("高学年")
 st.write(' ')
 moji3 = Image.open('images/iro3.jpg')
 st.image(moji3,width = 50)
