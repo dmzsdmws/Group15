583cfec7f8d673d23e4699f8d950ac6d1b151ca5
dmzsdmws <103725364+dmzsdmws@users.noreply.github.com>
2023-01-16 18:26:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 import pandas as pd
-st.title("      GROUP15 -IROFUDE-")
+st.title("   "  + "GROUP15 -IROFUDE-")
 st.title(" ")
+moji = st.text_input("検索したい文字",key="moji",value = "irofude")
+if st.session_state.moji is not None:
+   search = moji 
+image = Image.open(search +".jpg")
+st.image(image,width = 200)
 st.write('低学年')
 moji1 = Image.open('images/iro1.jpg')
 st.image(moji1,width = 50)
@@ -12,13 +17,10 @@ st.write('中学年')
 moji2 = Image.open('images/iro2.jpg')
 st.image(moji2,width = 50)
 st.write('高学年')
+st.write(' ')
 moji3 = Image.open('images/iro3.jpg')
 st.image(moji3,width = 50)
 
 # ---------- テキスト入力 ----------
 search = "white"
-moji = st.text_input("検索したい文字",key="moji",value = "irofude")
-if st.session_state.moji is not None:
-   search = moji 
-image = Image.open(search +".jpg")
-st.image(image,width = 200)
+
