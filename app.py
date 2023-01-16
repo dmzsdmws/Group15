c28f54f048e8dcb3caa9a81ca98a48eb3d5bfcaa
dmzsdmws <103725364+dmzsdmws@users.noreply.github.com>
2023-01-16 19:28:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import numpy as np
 import pandas as pd
 css = '''
 .stApp {
-    background: url('https://github.com/dmzsdmws/group15/blob/main/irofude.jpg');
+    background: url('https://wallpapercave.com/uwp/uwp3212116.png');
     background-size: cover;
     background-position: center;
 }
