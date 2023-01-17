ed4ba7991968765c4b5b7de6c1e6e888dc0bf133
dmzsdmws <103725364+dmzsdmws@users.noreply.github.com>
2023-01-18 01:58:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -24,21 +24,6 @@ css = '''
 
 '''
 
-st.title('Counter Example using Callbacks with args')
-if 'count' not in st.session_state:
-    st.session_state.count = 0
-
-increment_value = st.number_input('Enter a value', value=0, step=1)
-
-def increment_counter(increment_value):
-    st.session_state.count += increment_value
-
-increment = st.button('Increment', on_click=increment_counter,
-    args=(increment_value, ))
-
-st.write('Count = ', st.session_state.count)
-
-
 st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 st.title("  ")
 st.markdown('_スマホの画面を横にしてください_')
