cf5effc214fba79c3bd0fea5093faed37b5254cb
dmzsdmws <103725364+dmzsdmws@users.noreply.github.com>
2023-01-17 22:09:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -36,8 +36,15 @@ else:
    st.write('Error')
 
 image = Image.open(search +".jpg")
+x = 200
+col1, col2 = st.columns(2)
+with col1:
+    st.markdown('**検索結果:**')
+with col2:
+    if st.button('画像拡大'):
+    　  x = 400
 st.markdown('**検索結果:**')
-st.image(image,width = 200)
+st.image(image,width = x)
 moji1 = Image.open('images/dai1.jpg')
 moji2 = Image.open('images/iro1.jpg')
 moji3 = Image.open('images/dan1.jpg')
