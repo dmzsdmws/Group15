61368890c3da4804790bd1e1e1a72d401694ff16
dmzsdmws <103725364+dmzsdmws@users.noreply.github.com>
2023-01-18 07:04:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ with col3:
     if st.button('復元'):
         x = 200
 
-st.image(image,width = x, output_format="PNG")
+st.image(image,width = x)
 
 moji1 = Image.open('images/dai1.jpg')
 moji2 = Image.open('images/iro1.jpg')
