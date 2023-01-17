65a4e9d6bd2c87d8481e0b2d828af8f921a929fc
dmzsdmws <103725364+dmzsdmws@users.noreply.github.com>
2023-01-17 17:42:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,8 @@ moji = st.text_input("検索したい文字:",key="moji",value = "irofude")
 if st.session_state.moji is not None:
    search = moji 
 image = Image.open(search +".jpg")
-st.markdown('_ 検索結果: _')
+st.markdown(' _ 検索結果:_ ')
+st.markdown('Streamlit is **_really_ cool**.')
 st.image(image,width = 200)
 moji1 = Image.open('images/dai1.jpg')
 moji2 = Image.open('images/dan1.jpg')
