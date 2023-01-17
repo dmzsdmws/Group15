4d0e5d147374f0ef6857c41aaa91a44266f17791
dmzsdmws <103725364+dmzsdmws@users.noreply.github.com>
2023-01-18 01:57:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,24 @@ css = '''
 
 '''
 
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+
+def increment_counter(increment_value=0):
+    st.session_state.count += increment_value
+
+def decrement_counter(decrement_value=0):
+    st.session_state.count -= decrement_value
+
+st.button('Increment', on_click=increment_counter,
+    kwargs=dict(increment_value=5))
+
+st.button('Decrement', on_click=decrement_counter,
+    kwargs=dict(decrement_value=1))
+
+st.write('Count = ', st.session_state.count)
+
+
 st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 st.title("  ")
 st.markdown('_スマホの画面を横にしてください_')
