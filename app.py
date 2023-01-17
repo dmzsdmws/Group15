186d6680bc7fcb1f0683f286fca62d5ccc16df9b
dmzsdmws <103725364+dmzsdmws@users.noreply.github.com>
2023-01-18 01:57:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -24,20 +24,17 @@ css = '''
 
 '''
 
+st.title('Counter Example using Callbacks with args')
 if 'count' not in st.session_state:
     st.session_state.count = 0
 
-def increment_counter(increment_value=0):
+increment_value = st.number_input('Enter a value', value=0, step=1)
+
+def increment_counter(increment_value):
     st.session_state.count += increment_value
 
-def decrement_counter(decrement_value=0):
-    st.session_state.count -= decrement_value
-
-st.button('Increment', on_click=increment_counter,
-    kwargs=dict(increment_value=5))
-
-st.button('Decrement', on_click=decrement_counter,
-    kwargs=dict(decrement_value=1))
+increment = st.button('Increment', on_click=increment_counter,
+    args=(increment_value, ))
 
 st.write('Count = ', st.session_state.count)
 
