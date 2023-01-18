68aea1c7f1d00ece1f4c96bd55a48c18f6151054
dmzsdmws <103725364+dmzsdmws@users.noreply.github.com>
2023-01-18 09:40:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ css = '''
 .stApp {
     
     background: url('https://wallpapercave.com/uwp/uwp3219962.jpeg');
-    background-size: 60%;
+    background-size: 50%;
     background-repeat: no-repeat;
     background-position: center;
     background-color: #C2EEFF;
